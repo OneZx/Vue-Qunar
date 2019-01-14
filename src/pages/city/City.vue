6e5a3c5cdc19c89582c078a5276edcaf93bcf5db
zxd <996029276@qq.com>
2019-01-14 14:37:18 +0800

    add

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,12 +1,52 @@
 <template>
-    <div><city-header></city-header></div>
+    <div>
+        <city-header></city-header>
+        <city-search :cities="cities"></city-search>
+        <city-list
+            :cities="cities"
+            :hot="hotCities"
+            :letter="letter"
+        ></city-list>
+    </div>
 </template>
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import axios from 'axios'
+
 export default {
     name: 'City',
     components: {
-        CityHeader
+        CityHeader,
+        CitySearch,
+        CityList
+    },
+    data() {
+        return {
+            cities: {},
+            hotCities: [],
+            letter: ''
+        }
+    },
+    methods: {
+        getCityInfo() {
+            axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc(res) {
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        },
+        handleLetterChange(letter) {
+            this.letter = letter
+        }
+    },
+    mounted() {
+        this.getCityInfo()
     }
 }
 </script>
